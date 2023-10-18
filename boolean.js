ddd44ab9d34c03166028315593f73ffc0c11fefd
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-18 14:29:44 +0530

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,20 @@
+// using boolean //
+let studententry=true;
+   if(studententry){
+    console.log("entrypass");
+   }
+    else{
+    console.log("entryfail ");
+   }
+
+let y=100;
+let z=200;
+
+if(y,z){
+   console.log(y>z);
+}
+else{
+   console.log(y<z);
+   }
+
+         
