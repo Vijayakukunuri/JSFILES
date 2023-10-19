5b9c7d440cfdbeded81855667b0f47420b192efc
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-19 10:37:54 +0530

    Add files via upload

---
+++ firstpro.js
@@ -0,0 +1,17 @@
+//    using conditional statements//
+        let a=5;
+        let b=6;
+        let c=a+b;
+        console.log(`sum of ${a} and ${b} is ${c}`);
+    if(a>=b){
+        console.log("false");
+    } 
+    else {
+        console.log("true")
+    }
+    if(c==0){
+      console.log("notdefined");
+    }
+    else{
+        console.log("print result");
+    }
