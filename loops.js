662cac2b14a503a50d434b30c682b382e789fb07
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-19 10:06:08 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,2 +1,19 @@
-// using of datatypes//
-var name=prompt("enter name:");
+// using for loop//
+let i=1
+for(i=1;i<=20;i++){
+    console.log(i);
+}
+// using while loop//
+let even=0
+while(even<=20){
+    console.log(even);
+    even+=2
+}
+// using do while loop//
+let odd=21
+do{
+    console.log(odd)
+    odd+=2;
+    
+}
+while(odd<=50)
