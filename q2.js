56e491ef793d372e7e1f782a0ed63e8a7365d319
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-23 22:03:49 +0530

    Add files via upload

---
+++ q2.js
@@ -0,0 +1,9 @@
+// with duplicates//
+let phrase="finding vowels in a phrase";
+// using for in//
+for(let i in phrase){
+    let c =phrase[i]
+    if(c==='a'||c==='e'||c==='i'||c==='o'||c==='u'){
+        console.log(c);
+    }
+}
