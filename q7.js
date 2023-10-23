ebd43e1fe184a05256218a599fa1af10afb03c32
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-23 22:05:09 +0530

    Add files via upload

---
+++ q7.js
@@ -0,0 +1,8 @@
+//to print the statement in alphabetic order...
+
+let statement = "alphabetical order";
+//to use below conditon to converst above into alphabetic order//
+
+let sortstatement = statement.split('').sort( ).join('');
+
+console.log(sortstatement);
