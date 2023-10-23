ffb26666d02937a2b871a93b63aa64d5aa0ea657
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-23 22:03:30 +0530

    Add files via upload

---
+++ test1loops.js
@@ -0,0 +1,22 @@
+
+let even=0;
+let odd=1;
+let i=1;
+do{
+    if(i %2 === 0 ){
+      even +=i;   
+    }
+    else{
+        odd*=i;
+    }
+    i++
+}    
+while(i<=100); 
+console.log("sum of even num:"+even); 
+console.log("product of odd:"+odd);  
+
+
+
+    
+   
+
