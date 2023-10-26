083e6ac6b85de7810e4d0c75e7b040220cca1fd6
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-26 14:42:55 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,28 @@
+// empty array//
+let student=[];
+ console.log(student);
+
+// standard array//
+let studentnum=[1,2,3,4,5,6,7,8,9,10];
+  console.log(studentnum);
+
+// object array//
+let stclass=[{1:10,2:15,3:20}];
+ console.log(stclass);
+ 
+// dynamic array//
+student.push("first","second");
+   console.log(student);
+ 
+student.pop["first"];
+  console.log(student);
+
+// nested array//
+let studentsesc=[[1,2],[3,4]];
+  console.log(studentsec);
+
+// mixed array//
+let combine=["vijaya",2,{section:"first"},true];
+  console.log(combine);
+
+// 
