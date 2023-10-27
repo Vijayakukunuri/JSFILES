3bf3e6092ba3fe6722fedd07f8bc0a4f1991f363
Vijayakukunuri <144308280+Vijayakukunuri@users.noreply.github.com>
2023-10-27 16:57:53 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,28 @@
+// standard function//
+// function without return type(but using console.log) without argumentr//
+function nani(){
+    console.log("i'm vijaya");
+};
+nani();
+// function with return type with argument//
+function vijju(sum){
+    return `using return ${sum}`;
+};
+let x=vijju("type");
+console.log(x);
+let y=vijju('of');
+console.log(y);
+// functions without return type with argument//
+function surya(sub){
+    console.log(`without return ${sub}`);
+
+};
+surya("type");
+// function with return type without argument//
+function durga(){
+    return `without argument`;
+
+};
+let w=durga();
+console.log(w);
+
